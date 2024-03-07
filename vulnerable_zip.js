6e04c0451663385ae8373ed76afbaea11c462de1
Henrique Gama <henrique.fontes@br.ey.com>
2024-03-07 09:52:21 -0300

    Update vulnerable_zip.js

--- vulnerable_zip.js
+++ vulnerable_zip.js
@@ -10,3 +10,7 @@ app.get('/example', upload.single('file'), (req, res) => {
         writer.write(zipEntry.getData().toString("utf8"));
     });
 });
+
+/* The following code is vulnerable to Zip Slip as it is constructing a path using an archive entry name. 
+This path is then used to copy a file without being validated first. 
+Therefore, it can be leveraged by an attacker to overwrite arbitrary files. */
