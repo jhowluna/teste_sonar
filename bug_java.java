aa39021f852a40dbb2c454b13f1c8cc6e37d22ee
Jhonatan Luna <Jhonatan.Luna@br.ey.com>
2024-03-11 10:56:05 -0300

    teste

--- bug_java.java
+++
@@ -1,8 +0,0 @@
-public class HelloWorld {
-    public static void main(String[] args) {
-        System.out.println("Hello, World!");
-
-        // Declaração de uma variável não utilizada - SonarQube deve detectar isso como um code smell.
-        int unusedVariable = 5;
-    }
-}
--- seed_non_compliant.kt
+++
@@ -1,7 +0,0 @@
-import java.security.SecureRandom
-
-val sr = SecureRandom()
-sr.setSeed(123456L) // Noncompliant
-val v = sr.nextInt()
-
-//The following code uses a cryptographically strong random number generator to generate data that is not cryptographically strong.
